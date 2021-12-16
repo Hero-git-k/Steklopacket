aa3a2f3cf3ea280575a482677a3c57505feaef66
Denis <82368652+Hero-git-k@users.noreply.github.com>
2021-12-16 20:55:33 +0300

    Add files via upload

---
+++ 25.10.2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _25._10._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            int b;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+
+            if
+                { comboBox1.SelectedIndex == 0
+                (a < 800) 
+            { int sum = 10600 + 1000; }
+            else { int sum = 10600 + 1500; }
+                if (a < 1300)
+                { int sum = 14600 + 1000; }
+                else { int sum = 14600 + 1500; }
+            }
+            }
+        }
+    }
+ 
